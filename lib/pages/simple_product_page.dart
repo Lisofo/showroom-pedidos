26dd51ab5d22bfbb7125c333922866379198f531
Gulduster <guilleboero740@gmail.com>
2024-10-23 16:09:02 -0300

    ...

---
+++ lib/pages/simple_product_page.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:photo_view/photo_view.dart';
+import 'package:photo_view/photo_view_gallery.dart';
+import 'package:provider/provider.dart';
+import 'package:showroom_maqueta/models/product.dart';
+import 'package:showroom_maqueta/providers/item_provider.dart';
+import 'package:showroom_maqueta/services/product_services.dart';
+
+class PaginaSimpleProducto extends StatefulWidget {
+  const PaginaSimpleProducto({super.key});
+
+  @override
+  State<PaginaSimpleProducto> createState() => _PaginaSimpleProductoState();
+}
+
+class _PaginaSimpleProductoState extends State<PaginaSimpleProducto> {
+
+  late String raiz = '';
+  Product productoNuevo = Product.empty();
+  late String almacen = '';
+  late String token = '';
+  late int currentIndex = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    cargarDatos();
+  }
+
+  cargarDatos() async{
+    almacen = context.read<ItemProvider>().almacen;
+    token = context.read<ItemProvider>().token;
+    raiz = context.read<ItemProvider>().raiz;
+    productoNuevo = await ProductServices().getSingleProductByRaiz(raiz, almacen, token);
+    setState(() {
+      
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    
+    final colores = Theme.of(context).colorScheme;
+    return Scaffold(
+      appBar: AppBar(
+          title: Text(
+            raiz,
+            style: TextStyle(
+              color: colores.onPrimary
+            ),
+          ),
+          backgroundColor: colores.primary,
+          iconTheme: const IconThemeData(
+            color: Colors.white
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              if (productoNuevo.imagenes.isNotEmpty) ... [
+                const SizedBox(height: 20,),
+                Text(productoNuevo.descripcion, style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+                const SizedBox(height: 20,),
+                Center(
+                  child: SizedBox(
+                    width: MediaQuery.of(context).size.width * 0.98,
+                    height: MediaQuery.of(context).size.height * 0.7,
+                    child: PhotoViewGallery.builder(
+                      itemCount: productoNuevo.imagenes.length,
+                      builder: (context, index) {
+                        return PhotoViewGalleryPageOptions(
+                          imageProvider: NetworkImage(productoNuevo.imagenes[index]),
+                        );
+                      },
+                      onPageChanged: (index) {
+                        setState(() {
+                          currentIndex = index;
+                        });
+                      },
+                      scrollPhysics: const BouncingScrollPhysics(),
+                      backgroundDecoration: BoxDecoration(
+                        color: Theme.of(context).canvasColor,
+                      ),
+                    ),
+                  ),
+                ),
+
+              ] else ... [
+                const Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      CircularProgressIndicator(),
+                      SizedBox(height: 20,),
+                      Text('Cargando...')
+                    ],
+                  ),
+                )
+              ]
+            ],
+          ),
+        ),
+    );
+  }
+}
